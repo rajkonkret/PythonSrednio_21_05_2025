90f2aa9a36838da2c178b62b893f2048d85f2ac2
rajkonkret <rajkonkret660@gmail.com>
2025-05-21 15:31:03 +0200

    [1 9] update fun3

--- day_1/fun3.py
+++ day_1/fun3.py
@@ -1,6 +1,9 @@
 # lambda - skrócony zapis funkcji
 # lambda ma return
 # funkcja anonimowa - mozliwosc uzycia funkcji w miejscu deklaracji
+import math
+from collections import Counter
+from functools import reduce, lru_cache
 
 
 def mnozenie(x, y):
@@ -76,3 +79,66 @@ print(r0)
 print(r1)
 # {'miasto': 'Toruń', 'ZIP': '00-000'}
 # {'miasto': 'Toruń', 'ZIP': '25-900'}
+
+
+# reduce()
+# ((((1+2)+3)+4)+5)
+liczby = [1, 2, 3, 4, 5]
+wynik = reduce(lambda a, b: a + b, liczby)
+print("Wynik:", wynik)  # Wynik: 15
+# 1 + 2 = 3
+# 3 + 3  = 6
+# 6 + 4 = 10
+# 10 + 5 = 15
+
+wynik = reduce(lambda a, b: a * b, liczby)
+print(wynik)  # 120
+
+dane = ["a", "b", "a", "c", "a", "b", "b", "b", "c", "a"]
+
+licznik = Counter(dane)
+print(licznik)
+# Counter({'a': 4, 'b': 4, 'c': 2})
+wyn = reduce(lambda a, b: a if licznik[a] >= licznik[b] else b, licznik)
+print(wyn)  # a
+
+listy = [[1, 2], [3, 4], [5], [6, 7]]
+flatten = reduce(lambda a, b: a + b, listy)
+print(flatten)  # [1, 2, 3, 4, 5, 6, 7]
+lista1 = [1, 2]
+lista2 = [3, 4]
+print(lista1 + lista2)  # [1, 2, 3, 4]
+
+liczby = [2, 4, 4, 4, 5, 5, 7, 9]
+suma, suma_danych, n = reduce(
+    lambda acc, x: (acc[0] + x, acc[1] + x ** 2, acc[2] + 1),
+    liczby,
+    (0, 0, 0)
+)
+
+avg = suma / n
+wariancja = (suma_danych / n) - (avg ** 2)
+odchylenie = math.sqrt(wariancja)  # pierwastek
+print(f"Średnia: {avg:.2f}, Odchylenie std: {odchylenie:.2f}")
+
+
+# Średnia: 5.00, Odchylenie std: 2.00
+
+
+# lru_cache
+
+@lru_cache(maxsize=1000)  # dekorator
+def fib_cached(n):
+    if n < 2:
+        return n
+    return fib_cached(n - 1) + fib_cached(n - 2)
+
+
+print(fib_cached(5))  # 5
+print(fib_cached.cache_info())  # CacheInfo(hits=3, misses=6, maxsize=1000, currsize=6)
+print(fib_cached(10))  # 55
+print(fib_cached.cache_info())  # CacheInfo(hits=9, misses=11, maxsize=1000, currsize=11)
+print(fib_cached(10))
+print(fib_cached.cache_info())
+fib_cached.cache_clear()  # CacheInfo(hits=10, misses=11, maxsize=1000, currsize=11)
+print(fib_cached.cache_info())  # CacheInfo(hits=0, misses=0, maxsize=1000, currsize=0)
