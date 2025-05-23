33896a8a4fa32f5bd17c9838a4a7b35cfbdf5b36
rajkonkret <rajkonkret660@gmail.com>
2025-05-23 09:51:17 +0200

    [3 2]add kl_11.py

---
+++ day_3/kl_11.py
@@ -0,0 +1,12 @@
+class Matematyka:
+
+    @staticmethod
+    def dodaj(a, b):
+        return a + b
+
+    @staticmethod
+    def odejmij(a, b):
+        return a - b
+
+
+print(Matematyka.dodaj(5, 6))  # 11
